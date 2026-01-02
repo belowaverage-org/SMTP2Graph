dee46f92789debdba4add0983ee2a8b50c356b10
krisdb2009 <dylan@belowaverage.org>
2026-01-02 17:11:20 -0500

    Figured out you can just send the MIME...

--- SMTP2Graph/Program.cs
+++ SMTP2Graph/Program.cs
@@ -3,16 +3,18 @@ using Azure.Identity;
 using System.Net.Sockets;
 using System.Net;
 using System.Text.RegularExpressions;
-using Microsoft.Graph.Models;
 using System.Text;
+using Microsoft.Kiota.Abstractions;
 
 Console.WriteLine("Initializing...");
 var mail_listener = new TcpListener(IPAddress.Any, 25);
 var scopes = new[] { "https://graph.microsoft.com/.default" };
+
 var options = new ClientSecretCredentialOptions
 {
     AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
 };
+
 var clientSecretCredential = new ClientSecretCredential(
     Environment.GetEnvironmentVariable("TENANT_ID"),
     Environment.GetEnvironmentVariable("CLIENT_ID"),
@@ -53,85 +55,55 @@ while (true)
 void HandleConnection(TcpClient client, Stream stream, StreamWriter sw, StreamReader sr)
 {
     string From = string.Empty;
-    List<string> To = new();
-    string Body = string.Empty;
-    string Subject = string.Empty;
-    BodyType BType = BodyType.Text;
+    string MIME = string.Empty;
     while (true)
     {
         var msg = sr.ReadLine();
         if (msg.StartsWith("HELO") || msg.StartsWith("EHLO"))
         {
+            Console.WriteLine("HELO");
             sw.Write("250 Whatchu want?\r\n");
             continue;
         }
         if (msg.StartsWith("MAIL FROM:"))
         {
+            Console.WriteLine("MAIL FROM");
             From = Regex.Match(msg, "<(.*)>").Groups[1].Value;
             sw.Write("250 You again?!\r\n");
             continue;
         }
         if (msg.StartsWith("RCPT TO:"))
         {
-            To.Add(Regex.Match(msg, "<(.*)>").Groups[1].Value);
+            Console.WriteLine("RCPT TO");
             sw.Write("250 Ok fine...\r\n");
             continue;
         }
         if (msg.StartsWith("DATA"))
         {
+            Console.WriteLine("DATA");
             sw.Write("354 And whats so important that you have to bother me...?\r\n");
-            (BType, Subject, Body) = ReadData(sr);
+            MIME = ReadMIME(sr);
             sw.Write("250 Oh yea, that was *very* important... SMH.\r\n");
+            Console.WriteLine("Sending to MS Graph...");
+            SendMessage(From, MIME);
+            Console.WriteLine("Sent.");
             continue;
         }
         if (msg.StartsWith("QUIT"))
         {
+            Console.WriteLine("QUIT");
             sw.Write("221 Bye, don't talk to me...\r\n");
             break;
         }
     }
+    Console.WriteLine("Closing connection...");
     client.Close();
-    Console.WriteLine("Sending to MS Graph...");
-    SendMessage(From, To, Subject, Body, BType);
     Console.WriteLine("Done.");
 }
 
-(BodyType, string, string) ReadData(StreamReader sr)
+string ReadMIME(StreamReader sr)
 {
-    string Subject = string.Empty;
-    string Body;
-    BodyType BType = BodyType.Text;
-    bool Base64 = false;
-    while (true)
-    {
-        var msg = sr.ReadLine();
-        if (msg.StartsWith("Subject:"))
-        {
-            Subject = Regex.Match(msg, "Subject: (.*)").Groups[1].Value;
-            continue;
-        }
-        if (msg.StartsWith("Content-Type:"))
-        {
-            if (msg.Contains("html")) BType = BodyType.Html;
-            continue;
-        }
-        if (msg == "Content-Transfer-Encoding: base64")
-        {
-            Base64 = true;
-            continue;
-        }
-        if (msg == string.Empty) 
-        {
-            Body = ReadBody(sr, Base64);
-            break;
-        }
-    }
-    return (BType, Subject, Body);
-}
-
-string ReadBody(StreamReader sr, bool Base64)
-{
-    string Body = string.Empty;
+    string MIME = string.Empty;
     while (true)
     {
         var msg = sr.ReadLine();
@@ -140,31 +112,22 @@ string ReadBody(StreamReader sr, bool Base64)
         {
             msg = ".";
         }
-        Body += msg;
+        MIME += msg + "\r\n";
     }
-    if (Base64)
-    {
-        Body = Encoding.UTF8.GetString(Convert.FromBase64String(Body));
-    }
-    return Body;
+    return MIME;
 }
 
-void SendMessage(string From, List<string> To, string Subject, string Body, BodyType BType)
+void SendMessage(string From, string MIME)
 {
-    List<Recipient> recipients = new();
-    foreach (string email in To)
-    {
-        recipients.Add(new() { EmailAddress = new() { Address = email } });
-    }
-    _ = graphClient.Users[From].SendMail.PostAsync(new()
-    {
-        Message = new()
-        {
-            From = new() { EmailAddress = new() { Address = From } },
-            ToRecipients = recipients,
-            Subject = Subject,
-            Body = new() { ContentType = BType, Content = Body }
-        },
-        SaveToSentItems = false
-    });
+    var mimeb64bytes = Encoding.UTF8.GetBytes(
+        Convert.ToBase64String(
+            Encoding.UTF8.GetBytes(MIME)
+        )
+    );
+    var request = new RequestInformation();
+    request.URI = new($"https://graph.microsoft.com/v1.0/users/{From}/sendMail");
+    request.HttpMethod = Method.POST;
+    request.Headers.Add("Content-Type", "text/plain");
+    request.Content = new MemoryStream(mimeb64bytes);
+    _ = graphClient.RequestAdapter.SendNoContentAsync(request);
 }
